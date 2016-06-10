63f7bec37bbecbeaab9beaa46b51d755aaf82141
Maria Altebarmakian <m.altebarmakian@gmail.com>
2016-06-10 13:08:28 -0400

    added fields to users

--- db/migrate/20160610152639_add_notification_frequency_to_users.rb
+++ db/migrate/20160610152639_add_notification_frequency_to_users.rb
@@ -1,5 +1,5 @@
 class AddNotificationFrequencyToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :notifificationFrequency, :integer
+    add_column :users, :notificationFrequency, :integer
   end
 end
